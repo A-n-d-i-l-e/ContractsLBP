bfa40d0887deb35cd42e8536f6a20947ae440b15
Abstract <ndlovuandile100@gmail.com>
2022-11-04 12:25:48 +0200

    first commit

---
+++ BColor.sol
@@ -0,0 +1,16 @@
+
+pragma solidity 0.5.12;
+
+contract BColor {
+    function getColor()
+        external view
+        returns (bytes32);
+}
+
+contract BBronze is BColor {
+    function getColor()
+        external view
+        returns (bytes32) {
+            return bytes32("BRONZE");
+        }
+}
